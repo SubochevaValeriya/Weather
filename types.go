3215b2d7f77d25358b927f2b7cb201eeebda711c
Valeriya Subocheva <subocheva.valeria@gmail.com>
2022-10-09 19:45:24 +0300

    Used types (structures) were added

---
+++ types.go
@@ -0,0 +1,15 @@
+package weather
+
+import "time"
+
+type Subscription struct {
+	Id               int       `json:"id" db:"id"`
+	City             string    `json:"city" db:"city"`
+	SubscriptionDate time.Time `json:"subscription_date" db:"subscription_date"`
+}
+
+type Weather struct {
+	City        string    `json:"name" db:"city"`
+	Temperature int       `json:"temperature" db:"temperature"`
+	Date        time.Time `json:"date" db:"date"`
+}
